d20769c6c7c2b4d73dba46e293fe7ee7c7c59597
Synkct <46335241+Synkct@users.noreply.github.com>
2022-11-10 12:38:38 +1100

    added functionality

--- tripleTversion1.py
+++ tripleTversion1.py
@@ -2,8 +2,31 @@ grid = [[0,0,0],
         [0,0,0],
         [0,0,0]]
 
-print (str(grid[0][0]) + "|" + str(grid[0][1]) + "|" + str(grid[0][2]))
-print ("-----")
-print (str(grid[1][0]) + "|" + str(grid[1][1]) + "|" + str(grid[1][2]))
-print ("-----")
-print (str(grid[2][0]) + "|" + str(grid[2][1]) + "|" + str(grid[2][2]))
+game = 1
+turn = 1
+
+while game == 1:
+    horizontal = int(input("Which column ")) - 1
+    vertical = int(input("Which row ")) - 1
+    grid[vertical][horizontal] = turn
+
+    print (str(grid[0][0]) + "|" + str(grid[0][1]) + "|" + str(grid[0][2]))
+    print ("-----")
+    print (str(grid[1][0]) + "|" + str(grid[1][1]) + "|" + str(grid[1][2]))
+    print ("-----")
+    print (str(grid[2][0]) + "|" + str(grid[2][1]) + "|" + str(grid[2][2]))
+
+    if grid[vertical][0] == grid[vertical][1] and grid[vertical][0] == grid[vertical][2]:
+        print (str(turn) + " wins!")
+        game = 0 
+    elif grid[0][horizontal] == grid[1][horizontal] and grid[0][horizontal] == grid[2][horizontal]:
+        print (str(turn) + " wins!")
+        game = 0 
+    elif grid[0][0] == grid[1][1] and grid[0][0] == grid[2][2] and grid[1][1] != 0:
+        print (str(turn) + " wins!")
+        game = 0 
+    elif grid[0][2] == grid[1][1] and grid[0][2] == grid[2][0] and grid[1][1] != 0:
+        print (str(turn) + " wins!")
+        game = 0     
+
+    turn *= -1
