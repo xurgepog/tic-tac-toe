385ec6881fbd30535f129ecc42c2b0f9f54f4fc6
Synkct <46335241+Synkct@users.noreply.github.com>
2022-11-10 12:18:12 +1100

    drafted output

--- tripleTversion1.py
+++ tripleTversion1.py
@@ -1,3 +1,9 @@
 grid = [[0,0,0],
         [0,0,0],
         [0,0,0]]
+
+print (str(grid[0][0]) + "|" + str(grid[0][1]) + "|" + str(grid[0][2]))
+print ("-----")
+print (str(grid[1][0]) + "|" + str(grid[1][1]) + "|" + str(grid[1][2]))
+print ("-----")
+print (str(grid[2][0]) + "|" + str(grid[2][1]) + "|" + str(grid[2][2]))
