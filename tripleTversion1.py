a7a38d7aba0a2bded9c9b3af48c49b0190c3a0ae
Synkct <46335241+Synkct@users.noreply.github.com>
2022-11-22 09:23:24 +1100

    added multiple plays functionality
    so you don't need to press the green flag everytime

--- tripleTversion1.py
+++ tripleTversion1.py
@@ -1,63 +1,74 @@
 import random
 import time
+
 class Boardsetup:
     def __init__(self):
-        self.grid = [[0,0,0],
-                    [0,0,0],
-                    [0,0,0]]
+        self.grid = [[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]]
 
-gameboard = Boardsetup()
+running = True
 
-game = True
-turn = 1
-count = 0
+def tictactoe():
+    gameboard = Boardsetup()
 
-players = input("1 player or 2 player? ")
-players = int(players)
+    game = True
+    turn = 1
+    count = 0
 
-while game == True:
-    choice = True
-    while choice == True:
-        if players == 2 or players == 1 and turn == -1:
-            horizontal = int(input("Which column ")) - 1
-            vertical = int(input("Which row ")) - 1
-            if gameboard.grid[vertical][horizontal] == 0:
-                gameboard.grid[vertical][horizontal] = turn
-                choice = False
-            else:
-                print ("Invalid location")
-        elif players == 1 and turn == 1:
-            horizontal = random.randint(0,2)
-            vertical = random.randint(0,2)
-            if gameboard.grid[vertical][horizontal] == 0:
+    players = input("1 player or 2 player? ")
+    players = int(players)
+    while game == True:
+        choice = True
+        while choice == True:
+            if players == 2 or players == 1 and turn == -1:
+                horizontal = int(input("Which column ")) - 1
+                vertical = int(input("Which row ")) - 1
+                if gameboard.grid[vertical][horizontal] == 0:
+                        gameboard.grid[vertical][horizontal] = turn
+                        choice = False
+                else:
+                    print("Invalid location")
+            elif players == 1 and turn == 1:
+                horizontal = random.randint(0, 2)
+                vertical = random.randint(0, 2)
+                if gameboard.grid[vertical][horizontal] == 0:
                     gameboard.grid[vertical][horizontal] = turn
-                    print ("thinking...")
+                    print("thinking...")
                     time.sleep(1)
                     choice = False
-            
 
-    print (str(gameboard.grid[0][0]) + "|" + str(gameboard.grid[0][1]) + "|" + str(gameboard.grid[0][2]))
-    print ("-----")
-    print (str(gameboard.grid[1][0]) + "|" + str(gameboard.grid[1][1]) + "|" + str(gameboard.grid[1][2]))
-    print ("-----")
-    print (str(gameboard.grid[2][0]) + "|" + str(gameboard.grid[2][1]) + "|" + str(gameboard.grid[2][2]))
+        print(str(gameboard.grid[0][0]) + "|" +
+              str(gameboard.grid[0][1]) + "|" + str(gameboard.grid[0][2]))
+        print("-----")
+        print(str(gameboard.grid[1][0]) + "|" +
+              str(gameboard.grid[1][1]) + "|" + str(gameboard.grid[1][2]))
+        print("-----")
+        print(str(gameboard.grid[2][0]) + "|" +
+              str(gameboard.grid[2][1]) + "|" + str(gameboard.grid[2][2]))
 
-    if gameboard.grid[vertical][0] == gameboard.grid[vertical][1] and gameboard.grid[vertical][0] == gameboard.grid[vertical][2]:
-        print (str(turn) + " wins!")
-        game = False 
-    elif gameboard.grid[0][horizontal] == gameboard.grid[1][horizontal] and gameboard.grid[0][horizontal] == gameboard.grid[2][horizontal]:
-        print (str(turn) + " wins!")
-        game = False
-    elif gameboard.grid[0][0] == gameboard.grid[1][1] and gameboard.grid[0][0] == gameboard.grid[2][2] and gameboard.grid[1][1] != 0:
-        print (str(turn) + " wins!")
-        game = False
-    elif gameboard.grid[0][2] == gameboard.grid[1][1] and gameboard.grid[0][2] == gameboard.grid[2][0] and gameboard.grid[1][1] != 0:
-        print (str(turn) + " wins!")
-        game = False 
-    
-    count += 1
-    if count == 9:
-        print ("it's a tie")
-        game = False
+        if gameboard.grid[vertical][0] == gameboard.grid[vertical][1] and gameboard.grid[vertical][0] == gameboard.grid[vertical][2]:
+            print(str(turn) + " wins!")
+            game = False
+        elif gameboard.grid[0][horizontal] == gameboard.grid[1][horizontal] and gameboard.grid[0][horizontal] == gameboard.grid[2][horizontal]:
+            print(str(turn) + " wins!")
+            game = False
+        elif gameboard.grid[0][0] == gameboard.grid[1][1] and gameboard.grid[0][0] == gameboard.grid[2][2] and gameboard.grid[1][1] != 0:
+            print(str(turn) + " wins!")
+            game = False
+        elif gameboard.grid[0][2] == gameboard.grid[1][1] and gameboard.grid[0][2] == gameboard.grid[2][0] and gameboard.grid[1][1] != 0:
+            print(str(turn) + " wins!")
+            game = False
 
-    turn *= -1
+        count += 1
+        if count == 9:
+            print("it's a tie")
+            game = False
+
+        turn *= -1
+
+while running == True:
+    tictactoe()
+    again = input("Want to play again?")
+    if again == 'No' or 'n' or 'N' or "no":
+        running = False
